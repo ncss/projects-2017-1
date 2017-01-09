fc762bb159044173abe95a2724daf64c0aaac92b
edtechl <edwardhenglay@hotmail.com>
2017-01-09 17:05:05 +1100

    test

---
+++ BucketList/login.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class User:
+
+    @staticmethod
+    def login(username, password):
+        cur = conn.execute('''
+          SELECT id
+          FROM User
+          WHERE username?
+          AND password=?
+        ''', (username, password))
+        row = cur.fetchone()
+        user_id = None if row is None else row['id']
+        conn.commit()
+        return user_id
+
+    @staticmethod
+    def find(username):
+        cur = conn.execute('''
+            SELECT *
+            FROM User
+            Where username=?''', (username,))
+        row = cur.fetchone()
+        if row is None:
+            raise UserNotFound('{} does not exist'.format(username))
+        return User(row[0]
+
+    @staticmethod
+    def create(username):
+        cur = conn.execute('''
+            INSERT INTO
