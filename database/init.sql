c5cc508d42ab3aeaf5be5a6f489d11394b885513
Isaac-F <itflow3r@gmail.com>
2017-01-10 11:07:49 +1100

    Clarify naming conventions

--- database/init.sql
+++ database/init.sql
@@ -1,21 +1,21 @@
-CREATE TABLE list (
+CREATE TABLE lists (
 	id INTEGER NOT NULL,
 	userid INTEGER NOT NULL,
 	title TEXT NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (userid) REFERENCES user(id),
+	FOREIGN KEY (userid) REFERENCES users(id),
 );
 
-CREATE TABLE listitem (
+CREATE TABLE items (
 	id INTEGER NOT NULL,
 	listid INTEGER NOT NULL,
 	text TEXT NOT NULL,
 	completed  INTEGER NOT NULL,
 	PRIMARY KEY (id),
-	FOREIGN KEY (lsitid) REFERENCES list(id)
+	FOREIGN KEY (listid) REFERENCES lists(id)
 );
 
-CREATE TABLE user (
+CREATE TABLE users (
 	id INTEGER NOT NULL,
 	username INTEGER NOT NULL,
 	password INTEGER NOT NULL,
