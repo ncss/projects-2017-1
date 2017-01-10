f637c050c27963bfba6d83a50ebf496392439a16
Sam <sam.merren@hotmail.com>
2017-01-10 12:45:13 +1100

    allow a guest to signup using the signup_handler
    Using the GET and POST methods, we created a signup_handler to check if
    repeat_password is the same as password and create a new user

--- app.py
+++ app.py
@@ -16,8 +16,10 @@ def login(request):
     elif method == 'POST':
         username = request.get_field('username')
         password = request.get_field('password')
-        if username == 'meme' and password == '123':
-            request.set_secure_cookie('user_id', '1')
+        #user = db.get(username)
+        #if user is not None
+        if username == 'meme' and password == '123': #user.password:
+            request.set_secure_cookie('user_id', '1') #user.id)
         request.redirect(r'/')
 
 def create(request):
@@ -44,6 +46,24 @@ def logout(request):
     request.clear_cookie('user_id')
     request.redirect(r'/')
 
+def signup_handler(request):
+    method = request.request.method
+    if method == 'GET':
+        with open("not_instagram.html") as f:
+            request.write(f.read())
+            return
+    elif method == 'POST':
+        username = request.get_field('username')
+        password = request.get_field('password')
+        repeat_password = request.get_field('repeat_password')
+        #To do, here is where we put the database
+        #user = db.get(username)
+        #if user is not None
+        #user.password:
+        user_id = '1'
+        request.set_secure_cookie('user_id', user_id) #user.id)
+        request.redirect(r'/')
+
 
 # GET /list/create - Call create screen
 # POST /list/create - Post list to server and redirects to created list.
@@ -56,5 +76,6 @@ server.register(r'/login', login)
 server.register(r'/list/create', create)
 server.register(r'/list/(\d+)/', list_handler)
 server.register(r'/logout', logout)
+server.register(r'/user/create', signup_handler)
 
 server.run()
