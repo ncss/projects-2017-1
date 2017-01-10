2b8fd6d8ead269317e38115c611dc46959949136
mpbagot <packof14eggies@gmail.com>
2017-01-10 15:11:15 +1100

    Make class behavior consistent + creation time for lists

--- sql_objects.py
+++ sql_objects.py
@@ -7,10 +7,14 @@ conn.row_factory = sqlite3.Row
 cur = conn.cursor()
 
 class List:
-    def __init__(self, title, uid, id):
+    def __init__(self, title, uid, id=None, created=None):
         self.title = title
         self.uid = uid
         self.id = id
+        self.created = created
+
+    def __str__(self):
+        return self.title
 
     def add(self):
         '''
@@ -18,8 +22,10 @@ class List:
         Throws an error if user doesn't exist
         or bucket object invalid
         '''
-        cur.execute('INSERT INTO lists (uid, title) VALUES (?, ?);', (self.uid, self.title))
+        self.created = datetime.datetime.now()
+        cur.execute('INSERT INTO lists (uid, title, created) VALUES (?, ?, ?);', (self.uid, self.title, self.created))
         conn.commit()
+        self.id = cur.lastrowid
 
     def update(self):
         '''
@@ -49,28 +55,40 @@ class List:
         Get the bucket data of a given user
         '''
         cur.execute('''
-                    SELECT title, userid, id
+                    SELECT title, userid, id, created
                     FROM lists
                     WHERE id = ?;
                     ''', (list_id,))
         row = cur.fetchone()
-        #title, uid, id = row
-        return List(*row)
-        #raise BucketNotFoundError("Bucket List of {} not found".format(user.name))
+        if row is not None:
+            title, uid, id, created = row
+            return List(title, uid, id, created)
+        return None
+
+    @staticmethod
+    def get_user_lists(user):
+        '''
+        Get the bucket lists of a given user
+        '''
+        cur.execute('''
+                    SELECT title, userid, id, created
+                    FROM lists
+                    WHERE userid = ?;
+                    ''', (user.id,))
+        lists = []
+        for row in cur:
+            title, uid, id, created = row
+            lists.append(List(title, uid, id, created))
+        return lists
 
 
 class Item:
-    def __init__(self, vals, id, list_id):
-        self.completed = False
-        self.text = ''
-        self.image = ''
-        for a in vals:
-            if isinstance(a, int):
-                self.completed = bool(a)
-            elif a.startswith('static/'):
-                self.image = a.strip()
-            else:
-                self.text = a.strip()
+    def __init__(self, list_id, completed=False, text=None, image=None, id=None):
+        self.completed = completed
+        if text is None and image is None:
+            raise ValueError('Either text or image must be declared!!!')
+        self.text = text
+        self.image = image
         self.id  = id
         self.list_id = list_id
 
@@ -84,8 +102,12 @@ class Item:
         '''
         Add a bucket list item to the database.
         '''
-        cur.execute('INSERT INTO items (listid, text, completed, image) VALUES (?, ?, ?, ?);', (self.list_id, self.text, int(self.completed), self.image))
+        cur.execute('''INSERT INTO items (listid, text, completed, image)
+                    VALUES (?, ?, ?, ?);''',
+                    (self.list_id, self.text, int(self.completed), self.image))
+
         conn.commit()
+        self.id = cur.lastrowid
 
     @staticmethod
     def get(item_id):
@@ -97,8 +119,10 @@ class Item:
                     FROM items WHERE id = ?;
                     ''', (item_id))
         row = cur.fetchone()
-        *val, list_id = row
-        return Item(val, item_id, list_id)
+        if row is not None:
+            completed, text, image, list_id = row
+            return Item(item_id, list_id, completed, text, image)
+        return None
 
     def search(self):
         pass
@@ -124,9 +148,9 @@ class Item:
         conn.commit()
 
 class User:
-    def __init__(self, name, passwd):
+    def __init__(self, name, passwd, id=None):
         self.name, self.password = (name, passwd)
-        self.id = None
+        self.id = id
 
     def __str__(self):
         return "User(username={}, passwdHash={})".format(self.name, self.password)
@@ -145,7 +169,7 @@ class User:
 
     def update(self):
         '''
-            Does this update the user's password?
+        Does this update the user's password?
         '''
         cur.execute('''
                     UPDATE users
@@ -163,6 +187,9 @@ class User:
         Deletes a user,
         !!!Check for authentication first!!!
         '''
+        for i in List.get_user_lists(self):
+            i.delete()
+
         cur.execute('''
                     DELETE FROM users
                     WHERE username = ?;
@@ -179,8 +206,20 @@ class User:
                         FROM users u
                         WHERE u.username = ?;''', (username,))
         row = cur.fetchone()
-        name, password, id = row
-        #try:
-        return User(name, password, id)
-        #except:
-            #return None
+        if row is not None:
+            name, password, id = row
+            return User(name, password, id)
+        return None
+
+user = User('mitchell', 'hello2')
+u2 = User.get("mitchell")
+print([tuple(a) for a in cur.execute("SELECT * FROM users").fetchall()])
+
+print([tuple(a) for a in cur.execute("SELECT * FROM users").fetchall()])
+user.add()
+l = List("Project Completion List", 0)
+i = Item(0,0, text="Finish this db api!")
+print(user)
+print(l)
+print(i)
+print([tuple(a) for a in cur.execute("SELECT * FROM users").fetchall()])
