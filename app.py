92767dc15f089acad7c21ea6045c23c0bcbc5a8a
Sam <sam.merren@hotmail.com>
2017-01-12 03:43:02 +1100

    Pass user_id variable to news-feed

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def index_handler(request):
         user = User.get_by_id(int(cookie))
         names = user.get_newsfeed()
         names = [user.get_by_id(a.userid).name for a in names]
-        request.write(render_template('news-feed.html', {'names':names, 'is_user' : is_authorised(request), 'title' : 'News Feed', 'user' : user.name}))
+        request.write(render_template('news-feed.html', {'user_id': user.id, 'names':names, 'is_user' : is_authorised(request), 'title' : 'News Feed', 'user' : user.name}))
 
 
 def login_handler(request):
